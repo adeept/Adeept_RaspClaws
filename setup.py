6baf4b860d38273352979397314fc564ac4f4d19
Adeept <support@adeept.com>
2019-08-05 13:41:24 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ for x in range(1,4):
 		break
 
 for x in range(1,4):
-	if os.system("sudo apt-get install python3-smbus") == 0:
+	if os.system("sudo apt-get install -y python3-smbus") == 0:
 		break
 
 for x in range(1,4):
@@ -79,7 +79,7 @@ for x in range(1,4):
 		break
 
 for x in range(1,4):
-	if os.system("sudo pip3 install opencv-contrib-python") == 0:
+	if os.system("sudo apt-get install libopencv-dev python3-opencv") == 0:
 		break
 
 for x in range(1,4):
