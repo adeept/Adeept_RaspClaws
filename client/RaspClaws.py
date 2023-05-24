34f478a303e69071f301bcb999e422ef3614b2dd
Devin <support@adeept.com>
2023-05-24 17:50:26 +0800

    update

--- client/RaspClaws.py
+++ client/RaspClaws.py
@@ -101,17 +101,17 @@ video_threading.start()									 #Thread starts
 def replace_num(initial,new_num):   #Call this function to replace data in '.txt' file
 	newline=""
 	str_num=str(new_num)
-	with open("ip.txt","r") as f:
+	with open("./ip.txt","r") as f:
 		for line in f.readlines():
 			if(line.find(initial) == 0):
 				line = initial+"%s" %(str_num)
 			newline += line
-	with open("ip.txt","w") as f:
+	with open("./ip.txt","w") as f:
 		f.writelines(newline)	#Call this function to replace data in '.txt' file
 
 
 def num_import(initial):			#Call this function to import data from '.txt' file
-	with open("ip.txt") as f:
+	with open("./ip.txt") as f:
 		for line in f.readlines():
 			if(line.find(initial) == 0):
 				r=line
